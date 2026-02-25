2a4bbba3ca777272c2448d07e892640e3c82bbeb
rutastefano72-hue <rutastefano72@gmail.com>
2026-02-25 21:01:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def open_position(symbol, side, size, leverage):
             "marginMode": "crossed",
             "marginCoin": "USDT",
 
-            "size": str(round(size, 2)),
+            "size": str(round(size, 5)),
 
             "side": "buy" if side == "buy" else "sell",
             "tradeSide": "open",
